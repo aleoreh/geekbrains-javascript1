7afbc9ddfcf1548caa8f72e247f88c4102cd3cb4
aleoreh <aleoreh@gmail.com>
2016-06-18 10:14:37 +0300

    Исправление DRY.
    Вывод происходит через отдельную функцию.

--- 02/js/script.js
+++ 02/js/script.js
@@ -1,26 +1,30 @@
 'use strict';
 
 function f(n) {
-    var res = function(){};
+    var res = '';
 
     var div3 = n%3 === 0;
     var div5 = n%5 === 0;
 
     if (div3 & div5) {
-        res = function() {console.log('FizzBuzz');};
+        res = 'FizzBuzz';
     } else if (div3) {
-        res = function() {console.log('Fizz');};
+        res = 'Fizz';
     } else if (div5) {
-        res = function() {console.log('Buzz');};
+        res = 'Buzz';
     } else {
-        res = function() {console.log(n);};
+        res = n.toString();
     }
 
     return res;
 }
 
+function whatToDo(x) {
+    console.log(x);
+}
+
 var n = 100;
 
 for (var i = 1; i <= n; i++) {
-    f(i)();
+    whatToDo(f(i));
 }
